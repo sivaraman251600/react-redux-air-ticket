43c4df804f787269f97f22f6983a793b2da6bad0
K <251600@NTTDATA.COM>
2022-07-06 20:39:08 +0530

    working on registration page

--- air-ticket/src/App.js
+++ air-ticket/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 import './App.css';
+import { AiFillHome } from "react-icons/ai"
+import { AiOutlineLogin } from "react-icons/ai"
+import Login from './components/Pages/Login_Page/Login';
+import Registration from './components/Pages/Registration_Page/Registration';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='navigation-wrapper'>
+      <nav className='navigation-bar'>
+        <BrowserRouter>
+        <Link to={"/"} className="home-link-tag"><AiFillHome/></Link>
+        <Link to={"/login"} className="login-link-tag"><AiOutlineLogin /></Link>
+        <Link to={"/registration"} className="register-link-tag">Register</Link>
+        <Routes>
+          <Route path='/' element={<></>}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/registration' element={<Registration />}/>
+        </Routes>
+        </BrowserRouter>
+      </nav>
+      </div>
     </div>
   );
 }
---
+++ air-ticket/src/components/Pages/Registration_Page/Registration.js
@@ -0,0 +1,132 @@
+import axios from "axios";
+import React, { useState } from "react";
+import "../Registration_Page/Registartion.css";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import { useNavigate } from "react-router";
+
+function Registration() {
+  const [registrationDetails, setRegistrationDetails] = useState({
+    fullName: "",
+    mobileno: 0,
+    emailid: "",
+    password: "",
+  });
+
+  const [status, setStatus] = useState("");
+  const [error, setError] = useState("");
+  const navigateToLogin = useNavigate();
+  const [show, setShow] = useState(false);
+  const [fieldEmptyError, setFieldEmptyError] = useState(false);
+
+  const formSubmit = () => {
+    if (
+      registrationDetails.fullName.trim() !== "" &&
+      registrationDetails.mobileno.toString().length !== 0 &&
+      registrationDetails.emailid.trim() !== "" &&
+      registrationDetails.password.trim() !== ""
+    ) {
+      setFieldEmptyError(false);
+      axios
+        .post("http://localhost:3001/RegistartionDetails", registrationDetails)
+        .then((res) => {
+          setStatus(res.status);
+          if (res.status === 201) {
+            setShow(true);
+          }
+        })
+        .catch((err) => setError(err.response.status));
+    } else {
+      setFieldEmptyError(true);
+    }
+  };
+
+  const navigateToLoginPage = () => {
+    navigateToLogin("/login");
+  };
+
+  return (
+    <div className="registration-wrapper">
+      <h1>Registration</h1>
+      {fieldEmptyError && <h4>Please Fill The Input Field</h4>}
+      <div className="form-wrapper">
+        <label htmlFor="fullname">Full Name</label>
+        <input
+          name="fullname"
+          type={"text"}
+          required
+          onChange={(e) =>
+            setRegistrationDetails({
+              ...registrationDetails,
+              fullName: e.target.value,
+            })
+          }
+        />{" "}
+        <br />
+        <label htmlFor="mobileno">Mobile Number</label>
+        <input
+          name="mobileno"
+          type={"number"}
+          required
+          onChange={(e) =>
+            setRegistrationDetails({
+              ...registrationDetails,
+              mobileno: e.target.value,
+            })
+          }
+        />{" "}
+        <br />
+        <label htmlFor="emailid">Email Id</label>
+        <input
+          name="emailid"
+          type={"email"}
+          required
+          onChange={(e) =>
+            setRegistrationDetails({
+              ...registrationDetails,
+              emailid: e.target.value,
+            })
+          }
+        />{" "}
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type={"password"}
+          required
+          onChange={(e) =>
+            setRegistrationDetails({
+              ...registrationDetails,
+              password: e.target.value,
+            })
+          }
+        />{" "}
+        <br />
+        <button
+          className="registration-page-submit-button"
+          onClick={() => formSubmit()}
+        >
+          SUBMIT
+        </button>
+      </div>
+      {show && (
+        <Dialog open={show} onClose={() => setShow(false)}>
+          <DialogTitle>{"Registartion Was Successful"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Please Click The Ok Button, Navigate To Login Page
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <button onClick={() => navigateToLoginPage()}>Ok</button>
+          </DialogActions>
+        </Dialog>
+      )}
+    </div>
+  );
+}
+
+export default Registration;
